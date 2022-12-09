71ecabc128940a1d9733fec8964e45246664dd4b
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-12-09 18:07:52 +0530

    Added Class Component
    Added Class Component and imported to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React, { Component } from 'react'
 import './App.css'; 
 import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Greet/>
+        <Welcome></Welcome>
       </div>
     )
   }
---
+++ src/Components/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to my website</h2>
+        <p>Im Learning REACT_JS</p>
+      </div>
+    )
+  }
+}
+
+export default Welcome;
